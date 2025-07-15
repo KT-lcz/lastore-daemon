d9e37f9b28efe26acf831f2b61c342475d1519a7
lichangze <lichangze@uniontech.com>
2025-07-15 10:31:13 +0800

    移除废弃的接口

--- src/internal/system/update_type.go
+++ src/internal/system/update_type.go
@@ -28,8 +28,8 @@ const (
 	AllCheckUpdate   = SystemUpdate | AppStoreUpdate | SecurityUpdate | UnknownUpdate | OtherSystemUpdate | AppendUpdate // 所有需要检查的仓库 TODO 该字段变动，需要检查所有使用者
 	AllInstallUpdate = SystemUpdate | SecurityUpdate | UnknownUpdate                                                     // 所有控制中心需要显示的仓库
 
-	OfflineUpdate UpdateType = 1 << 6 // 离线仓库，控制中心不检查、不显示；离线更新工具检查和显示；
-	AppendUpdate  UpdateType = 1 << 7 // 追加仓库/etc/deepin/lastore-daemon/sources.list.d/ 用于打印管理追加离线包.检查该仓库,不显示更新内容
+	//OfflineUpdate UpdateType = 1 << 6 // 离线仓库，控制中心不检查、不显示；离线更新工具检查和显示；
+	AppendUpdate UpdateType = 1 << 7 // 追加仓库/etc/deepin/lastore-daemon/sources.list.d/ 用于打印管理追加离线包.检查该仓库,不显示更新内容
 )
 
 func (m UpdateType) JobType() string {
@@ -42,8 +42,8 @@ func (m UpdateType) JobType() string {
 		return SecurityUpgradeJobType
 	case UnknownUpdate:
 		return UnknownUpgradeJobType
-	case OfflineUpdate:
-		return OfflineUpgradeJobType
+	//case OfflineUpdate:
+	//	return OfflineUpgradeJobType
 	case OtherSystemUpdate:
 		return OtherUpgradeJobType
 	case AppendUpdate:
@@ -70,7 +70,7 @@ func AllUpdateType() []UpdateType {
 		SecurityUpdate,
 		UnknownUpdate,
 		OtherSystemUpdate,
-		OfflineUpdate,
+		//OfflineUpdate,
 		AppendUpdate,
 	}
 }
@@ -138,8 +138,8 @@ func GetCategorySourceMap() map[UpdateType]string {
 		SecurityUpdate:    SecuritySourceDir,
 		UnknownUpdate:     UnknownSourceDir,
 		OtherSystemUpdate: OtherSystemSourceDir,
-		OfflineUpdate:     OfflineSourceFile,
-		AppendUpdate:      AppendSourceDir,
+		//OfflineUpdate:     OfflineSourceFile,
+		AppendUpdate: AppendSourceDir,
 	}
 }
 
@@ -317,9 +317,6 @@ func CustomSourceWrapper(updateType UpdateType, doRealAction func(path string, u
 			sourcePath := GetCategorySourceMap()[t]
 			sourcePathList = append(sourcePathList, sourcePath)
 		}
-	}
-	if updateType&OfflineUpdate != 0 {
-		sourcePathList = append(sourcePathList, GetCategorySourceMap()[updateType])
 	}
 	// 由于103x版本兼容，检查更新时需要检查商店仓库
 	// if updateType&AppStoreUpdate != 0 {
